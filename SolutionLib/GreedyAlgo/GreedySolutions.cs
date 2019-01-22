182c29659720237602b8420c152217e0b4bdb001
sercantimocin <sercantimocin@gmail.com>
2019-01-22 22:42:10 +0300

    greedy and search

---
+++ SolutionLib/GreedyAlgo/GreedySolutions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionLib.GreedyAlgo
+{
+    public class GreedySolutions
+    {
+        //https://www.hackerrank.com/challenges/greedy-florist/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
+        static int getMinimumCost(int k, int[] c)
+        {
+            Array.Sort(c);
+
+            int cost = 0;
+            int initialCost = 1;
+            int counter = 0;
+
+            for (int i = c.Length - 1; i > -1; i--)
+            {
+                cost += (c[i] * initialCost);
+                counter++;
+                if ((counter) % k == 0)
+                {
+                    initialCost++;
+                }
+            }
+
+            return cost;
+        }
+    }
+}
---
+++ SolutionLib/Search/SearchSolutions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionLib.Search
+{
+    public class SearchSolutions
+    {
+        public class Node
+        {
+            public int data;
+            public Node rigth;
+            public Node left;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+
+
+        static int[][] swapNodes(int[][] indexes, int[] queries)
+        {
+
+            var root = new Node(1);
+
+            Queue<Node> parentQueue = new Queue<Node>();
+            parentQueue.Enqueue(root);
+
+            int row = 0;
+            while (parentQueue.Count > 0)
+            {
+                var parent = parentQueue.Dequeue();
+
+                int rData = indexes[row][0];
+                int lData = indexes[row][1];
+
+                if (rData != -1)
+                {
+                    parent.rigth = new Node(rData);
+                    parentQueue.Enqueue(parent.rigth);
+                }
+
+                if (lData != -1)
+                {
+                    parent.left = new Node(lData);
+                    parentQueue.Enqueue(parent.left);
+                }
+            }
+
+            //Node current = root;
+            //int currentDepth = 1;
+
+            //while (current != null)
+            //{
+            //    current
+            //    current.left
+            //}
+
+
+
+            //int i = 0;
+            //int j = 0;
+            //int depth = queries[i];
+            //int currentDepth = 0;
+
+            //LinkedList<object> list = new LinkedList<object>();
+            //list.AddFirst(root);
+
+
+            //while (list.Count > 0)
+            //{
+            //    var parent = list.First;
+            //    list.RemoveFirst();
+            //    currentDepth++;
+
+            //    if (parent is Node)
+            //    {
+            //        if (currentDepth == depth)
+            //        {
+            //            var temp = parent.left;
+            //            parent.left = parent.rigth;
+            //            parent.rigth = temp;
+            //        }
+
+            //        parent.AddFirst(current.left);
+            //        parent.AddFirst(current);
+            //        parent.AddFirst(current.right);
+            //    }
+            //    else
+            //    {
+            //        currentDepth--;
+
+            //        indexes[i][j] = parent as int;
+            //        j++;
+            //    }
+            //}
+
+
+
+            return indexes;
+        }
+
+    }
+}
