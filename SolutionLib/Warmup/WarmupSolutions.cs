dbb60bc4276de12f104b663921a7e4d9c702e662
sercantimocin <sercantimocin@gmail.com>
2019-02-04 21:12:26 +0300

    sockmerchant

--- SolutionLib/Warmup/WarmupSolutions.cs
+++ SolutionLib/Warmup/WarmupSolutions.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace SolutionLib.Warmup
 {
-    class WarmupSolutions
+    public static class WarmupSolutions
     {
+        //https://www.hackerrank.com/challenges/sock-merchant/problem
+        static int sockMerchant(int n, int[] ar)
+        {
+            var map = new Dictionary<int, int>();
+
+            foreach (int item in ar)
+            {
+                if (map.ContainsKey(item))
+                {
+                    map[item]++;
+                }
+                else
+                {
+                    map.Add(item, 1);
+                }
+            }
+
+            int count = 0;
+            foreach (var m in map)
+            {
+                count += (m.Value / 2);
+            }
+
+            return count;
+        }
     }
 }
